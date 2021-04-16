7c930c41467fcc585ccab618c61ce9cae978e92a
yuus0524 <sy040611@icloud.com>
2021-04-16 18:12:48 +0900

    レコードの取得を1つにまとめてメソッド化

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :destroy]
+  before_action :set_product, only: [:show, :destroy]
 
   def index
     @products = Product.includes(:user).order('created_at DESC')
@@ -19,11 +20,9 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
   def destroy
-    product = Product.find(params[:id])
     if current_user.id == product.user_id
       product.destroy
       redirect_to root_path
@@ -35,5 +34,9 @@ class ProductsController < ApplicationController
   def product_params
     params.require(:product).permit(:image, :product_name, :description, :category_id, :condition_id, :delivery_burden_id,
                                     :prefecture_id, :days_to_ship_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_product
+    @product = Product.find(params[:id])
   end
 end
