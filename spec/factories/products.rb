12081cdf4d3e2cb5812f97281d3fa986af8a2af3
yuus0524 <sy040611@icloud.com>
2021-04-16 12:09:38 +0900

    FactoryBotにおいて数値として取り扱うカラムのクオテーション削除

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :product do
     product_name {'test'}
     description {'test'}
-    category_id {'2'}
-    condition_id {'2'}
-    delivery_burden_id {'2'}
-    prefecture_id {'2'}
-    days_to_ship_id {'2'}
-    price {'1000'}
+    category_id {2}
+    condition_id {2}
+    delivery_burden_id {2}
+    prefecture_id {2}
+    days_to_ship_id {2}
+    price {1000}
 
     association :user
 
